df004899dfff96dcec1d6b0eeed13ef73c24a018
Antonio Mistretta <antonio.f.mistretta@gmail.com>
2023-09-29 18:30:14 -0500

    updated the general layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      display: ['Parisienne', 'serif'],
+    },
+  },
+  plugins: [],
+}
