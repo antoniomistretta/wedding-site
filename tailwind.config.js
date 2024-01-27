1bae6edb4719ed6ec9878c31ea9baa57454d3efd
Antonio Mistretta <antonio.f.mistretta@gmail.com>
2024-01-27 11:15:53 -0600

    add sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
+      'sans': ['"Noto Sans"'],
       'serif': ['"Noto Serif"'],
       'cursive': ['"Pinyon Script"']
     }
